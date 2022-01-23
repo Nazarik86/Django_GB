c02dee176080f2df66150cf3769878bdfae4df3c
Mikhail <mikhail.nazarov@internet.ru>
2022-01-24 00:20:14 +0300

    Second lesson is Done

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6v_&(p+fi9r3_j(sm)-8d9q^vhi9)+_kip+a^o&+a0ak$m!7(6'
+SECRET_KEY = 'django-insecure-s=6!7v6-q%05e4ielw%0+a2)=wffd*aq!62hwrbbh!a0dddt+j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,13 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
